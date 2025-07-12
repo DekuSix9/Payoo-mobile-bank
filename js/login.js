f877cf47ced04589b17575928b22b55940f73912
shamim <shamim@gmail.com>
2025-07-13 00:34:40 +0600

    handle login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+
+document.getElementById('btn-login').addEventListener('click',function(event){
+    event.preventDefault();
+
+ const phoneNumber=document.getElementById('phone-number').value;
+ const pinNumber=document.getElementById('pin-number').value;
+
+ if(phoneNumber==='5' && pinNumber==='1234'){
+    window.location.href='/home.html'
+ }
+else{
+    alert('Wrong phone number or pin.')
+}
+
+
+})
